0ca50f5f75134856843b41e8c4329090225f2442
Tristan Brown <tgbrown@stanford.edu>
2018-07-31 23:21:01 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,5 +1,10 @@
 def position_taken?(board, index)
   if board[index] == " "
     return false
+  elsif board[index] == ""
+    return false
+  elsif board[index] == nil
+  else
+    return true 
 end
-end 
+end
